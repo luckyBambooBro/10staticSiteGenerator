82367af176cf731bcc7640da38fa0bfbc9106367
luckyBambooBro <huypham008@hotmail.com>
2025-09-02 08:16:00 +0930

    up to ch2, l4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,38 +1,22 @@
 import unittest
 from htmlnode import HTMLNode
 
-class htmlTest(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_tag(self):
-        node = HTMLNode("<p>")
-        node2 = HTMLNode("<p>")
+        node = HTMLNode("p")
+        node2 = HTMLNode("p")
         self.assertEqual(node, node2)
 
-    def test_value(self):
-        node = HTMLNode("<h1>", "lorem ipsum")
-        node2 = HTMLNode("<h1>", "lorem ipsum")
-        self.assertEqual(node, node2)
+    def test_values(self):
+        node = HTMLNode("h1", "lorem ipsum", [HTMLNode("p", None, None, None)], {"href": "https://google.com"})
+        self.assertEqual(node.tag, "h1" )
+        self.assertEqual(node.value, "lorem ipsum")
+        self.assertEqual(node.children, [HTMLNode("p", None, None, None)])
+        self.assertEqual(node.props, {"href": "https://google.com"})
 
-    def test_different_value(self):
-        node = HTMLNode("<h1>", "lorem ipsum")
-        node2 = HTMLNode("<h1>", "lorem ipsum lorem ipsum")
-        self.assertNotEqual(node, node2)
+    def test_props_to_html(self):
+        node = HTMLNode(None, None, None, {"href": "https://youtube.com"})
+        self.assertEqual(node.props_to_html(), " href=\"https://youtube.com\"")
 
-    def test_same_children(self):
-        node = HTMLNode("<body>", "lorem ipsum", ["<h1>", "<h2>"])
-        node2 = HTMLNode("<body>", "lorem ipsum", ["<h1>", "<h2>"])
-        self.assertEqual(node, node2)
-
-    def test_different_children(self):
-        node = HTMLNode("<body>", "lorem ipsum", ["<h1>", "<h2>"])
-        node2 = HTMLNode("<body>", "lorem ipsum", ["<h3>", "<h4>"])
-        self.assertNotEqual(node, node2)
-
-    def test_same_props(self):
-        node = HTMLNode("<a>", None, None, {"href": "https://google.com"})
-        node2 = HTMLNode("<a>", None, None, {"href": "https://google.com"})
-        self.assertEqual(node, node2)
-
-    def test_different_props(self):
-        node = HTMLNode("<a>", None, None, {"href": "https://google.com"})
-        node2 = HTMLNode("<a>", None, None, {"href": "https://youtube.com"})
-        self.assertNotEqual(node, node2)
+    if __name__ == "__main__":
+        unittest.main()
